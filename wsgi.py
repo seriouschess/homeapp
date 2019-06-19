e6819f77ba9641811b6ca30863baa3ddcb139279
Brandon Rehayem <rehayemb@gmail.com>
2019-06-19 09:41:49 -0500

    potential fix for uwsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from homeapp import app as application
+from homeapp import app as app #"as application" has been suggested, no idea why
 from db import db
 
 db.init_app(app)
@@ -6,3 +6,4 @@ db.init_app(app)
 @app.before_first_request #first thing the server does
 def create_tables():
     db.create_all() #creates data.db unless it exists already
+:
